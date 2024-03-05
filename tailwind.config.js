0d96ac0050f8ebb7f47286e67416f9fd00011a9d
rojhat <rojhatbirel43821@gmail.com>
2024-03-06 00:15:00 +0300

    Yeni dosyalar ve stil değişiklikleri eklendi

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'regal-blue': '#243c5a',
+        'transparent': 'transparent',
+        'transparent-gray': '#53535366',
+        'transparent-grayhvr': '#535353',
+        'regal-red': '#d62828',
+      },
+      fontFamily: {
+      }
+    },
+  },
+  plugins: [],
+}
